4249deb480266bf5f579a5e73fa87ef5c9240659
Nick <ndondzila@gmail.com>
2020-06-18 13:25:26 -0400

    cosmetic edit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Meow. Hi Juliet :)</h1>
+        <hr />
+        <br />
         <h2>Also, hi Mike.  Deploying React on AWS is pretty simple; so I simply need to practice general React concepts to be prepared for the proof of concept this weekend.</h2>
       </header>
     </div>
